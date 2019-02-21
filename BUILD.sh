f3902b030a4487deaf06002956bdc711c09cbb4c
Eli Heuer <eliheuer@gmail.com>
2019-02-20 22:34:26 -0500

    Change flag layout

--- BUILD.sh
+++ BUILD.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 python3 sources/BUILD.py \
+    --ttfautohint "-v --stem-width-mode=qsq" \
     --static \
     --googlefonts ~/Google/fonts/ofl/dosis \
     --fontbakery \
-    --ttfautohint "-v --stem-width-mode=qsq" \
     && mv ~/Google/fonts/ofl/dosis/Dosis-VF.ttf ~/Google/fonts/ofl/dosis/Dosis-Regular.ttf
 #    --drawbot \
 #    --addfont \
